eb4ac10bc502e769c5ce51e490ae85f2f8f97e8f
shabbosKestenbaum <l.cotrino@uniandes.edu.co>
2026-03-02 19:09:27 +0000

    javascript y animaciones

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log("helloWorld!");
+
+const varTitulo = document.getElementById("titulo");
+
+varTitulo.addEventListener("click", () => {
+    varTitulo.style.color = "red";
+    console.log("titulo clickeado");
+});
