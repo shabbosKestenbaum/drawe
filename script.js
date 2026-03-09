287f1747106fdfdb68e76c5ca64fa549f8edc835
shabbosKestenbaum <l.cotrino@uniandes.edu.co>
2026-03-09 16:01:19 +0000

    javascript

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 //console.log("helloWorld!");
 
 const varTitulo = document.getElementById("titulo");
+const varTexto = document.getElementById("texto");
+
+const listaTexos = ["uno", "dos", "tres", "cuatro", "cinco"];
+
+let estadoInicial = true;
+let indice = 0;
 
 varTitulo.addEventListener("click", () => {
-    varTitulo.style.color = "red";
-    console.log("titulo clickeado");
+    if (estadoInicial === true) {
+        varTitulo.innerText = "Buenos días!";
+        varTitulo.style.color = "green";
+        varTitulo.style.backgroundColor = "white";
+    }
+        else {
+            varTitulo.innerText = "Hola Mundo!";
+            varTitulo.style.color = "red";
+            varTitulo.style.backgroundColor = "transparent";
+        }
+
+    estadoInicial = !estadoInicial;
+});
+
+varTexto.addEventListener("click", () => {
+    if(indice < listaTexos.length) {
+        varTexto.innerText = listaTexos[indice];
+        indice = indice + 1;
+    } else {
+        varTexto.innerText = "FIN.";
+    }
 });
